6fbdcbb0cb8aef1b019e0daec8578e6e03d0e431
Diks19 <andhika1910@gmail.com>
2022-03-23 05:49:34 +0700

    menambah dan merubah beberapa file

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -14,14 +14,20 @@
                             </div>
                             <div class="navbar-wrap main-menu d-none d-lg-flex">
                                 <ul class="navigation">
-                                    @yield('header')
+                                    <li class="{{ ($navlink == 'home') ? 'active' : '' }}"><a href="/">home</a></li>
+                                    <li class="{{ ($navlink == 'movies') ? 'active' : '' }}"><a href="/movies">movies</a></li>
+                                    <li class="{{ ($navlink == 'series') ? 'active' : '' }}"><a href=/series>series</a></li>
+                                    <li class="{{ ($navlink == 'about') ? 'active' : '' }}"><a href="/about">about us</a></li>
                                 </ul>
                             </div>
                             <div class="header-action d-none d-md-block">
                                 <ul>
                                     @if (Route::has('login'))
                                         @auth
-                                        <li class="header-btn"><a href="route('logout')" onclick="event.preventDefault(); this.closest('form').submit();" class="btn">Log Out</a></li>
+                                        <li class="header-btn"><a href="{{ route('logout') }}"  onclick="event.preventDefault();document.getElementById('logout-form').submit();" class="btn">Log Out</a></li>
+                                        <form method="POST" id="logout-form" action="{{ route('logout') }}" style="display: none">
+                                            @csrf
+                                        </form>
                                         @else
                                         <li class="header-btn"><a href="{{ route('login') }}" class="btn">Log In</a></li>
                                         @endauth
