5d32364d93c77d8fbdc7686534742abad07e9ce2
HaruSasaki <73406832+HaruSasaki@users.noreply.github.com>
2022-03-22 17:36:23 +0700

    add carousel to banner

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,34 +9,50 @@
 @endsection
 
 @section('main')
-<div class="banner-area banner-bg" data-background="img/banner/banner_bg01.jpg">
-    <div class="container custom-container">
-        <div class="row">
-            <div class="col-xl-6 col-lg-8">
-                <div class="banner-content">
-                    <h6 class="sub-title wow fadeInUp" data-wow-delay=".2s" data-wow-duration="1.8s">Moovy</h6>
-                    <h2 class="title wow fadeInUp" data-wow-delay=".4s" data-wow-duration="1.8s">Unlimited <span>Movie</span>, TVs Shows, & More.</h2>
-                    <div class="banner-meta wow fadeInUp" data-wow-delay=".6s" data-wow-duration="1.8s">
-                        <ul>
-                            <li class="quality">
-                                <span>Pg 18</span>
-                            </li>
-                            <li class="category">
-                                <a href="#">Romance,</a>
-                                <a href="#">Drama</a>
-                            </li>
-                            <li class="release-time">
-                                <span><i class="far fa-calendar-alt"></i> 2021</span>
-                                <span><i class="far fa-clock"></i> 128 min</span>
-                            </li>
-                        </ul>
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <div class="banner-area banner-bg" data-background="img/banner/banner_bg01.jpg">
+            <div class="container custom-container">
+                <div class="row">
+                    <div class="col-xl-6 col-lg-8">
+                        <div class="banner-content">
+                            <h2 class="title wow fadeInUp" data-wow-delay=".4s" data-wow-duration="1.8s">Unlimited <span>Blade</span> Works</h2>
+                            <div class="banner-meta wow fadeInUp" data-wow-delay=".6s" data-wow-duration="1.8s">
+                                <ul>
+                                    <li class="quality">
+                                        <span>Pg 18</span>
+                                    </li>
+                                    <li class="category">
+                                        <a href="#">Romance,</a>
+                                        <a href="#">Drama</a>
+                                    </li>
+                                    <li class="release-time">
+                                        <span><i class="far fa-calendar-alt"></i> 2021</span>
+                                        <span><i class="far fa-clock"></i> 128 min</span>
+                                    </li>
+                                </ul>
+                            </div>
+                            <a href="https://www.youtube.com/watch?v=R2gbPxeNk2E" class="banner-btn btn popup-video wow fadeInUp" data-wow-delay=".8s" data-wow-duration="1.8s"><i class="fas fa-play"></i> Watch Trailer</a>
+                        </div>
                     </div>
-                    <a href="https://www.youtube.com/watch?v=R2gbPxeNk2E" class="banner-btn btn popup-video wow fadeInUp" data-wow-delay=".8s" data-wow-duration="1.8s"><i class="fas fa-play"></i> Watch Trailer</a>
                 </div>
             </div>
         </div>
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="..." alt="Second slide">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="..." alt="Third slide">
+      </div>
     </div>
-</div>
+  </div>
 
 <!-- top-rated-movie -->
         <div class="ucm-area ucm-bg" data-background="img/bg/ucm_bg.jpg">
