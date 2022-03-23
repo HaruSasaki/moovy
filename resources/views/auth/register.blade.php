ed975012b7d61b0a22513200baf3e27baaf88b0d
Diks19 <andhika1910@gmail.com>
2022-03-23 12:02:53 +0700

    update file

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,59 +1,38 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
-
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
-
-        <form method="POST" action="{{ route('register') }}">
-            @csrf
-
-            <!-- Name -->
-            <div>
-                <x-label for="name" :value="__('Name')" />
-
-                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
+@extends('layouts.main')
+@section('main')
+<div class="contact-area contact-bg">
+    <div class="container">
+        <div class="row d-flex justify-content-center">
+            <div class="col-xl-6 col-lg-7">
+                <div class="widget-title mb-50">
+                <div class="row">
+                        <div class="col"><h5 class="title"><a href="/login">Login</a></h5></div>
+                        <div class="col"><h5 class="title"><a href="/register">Register</a></h5></div>
+                    </div>
+                </div>
+                    <div class="contact-form">
+                        <form action="{{ route('register') }}" method="POST">
+                            @csrf
+                            <div class="col-md-12">
+                                <input type="text" name="name" placeholder="Name">
+                            </div>
+                            <div class="col-md-12">
+                                <input type="email" name="email" placeholder="E-Mail">
+                            </div>
+                            <div class="col-md-12">
+                                <input type="password" name="password" placeholder="Password">
+                            </div>
+                            <div class="col-md-12">
+                                <input type="password" name="password_confirmation" placeholder="Confirm Password">
+                            </div>
+                            <div class="d-flex justify-content-end">
+                                <button class="btn">Submit</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
-
-            <!-- Email Address -->
-            <div class="mt-4">
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
-            </div>
-
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
-
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="new-password" />
-            </div>
-
-            <!-- Confirm Password -->
-            <div class="mt-4">
-                <x-label for="password_confirmation" :value="__('Confirm Password')" />
-
-                <x-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
-
-                <x-button class="ml-4">
-                    {{ __('Register') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+        </div>
+    </div>
+</div>
+@endsection
