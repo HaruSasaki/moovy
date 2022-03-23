a596b0a738ac86d2c7bba3107e5b8847a310b7cd
HaruSasaki <73406832+HaruSasaki@users.noreply.github.com>
2022-03-23 09:54:21 +0700

    Update about.blade.php

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -8,51 +8,51 @@
                 <div class="blog-post-item blog-details-wrap">
                     <div class="blog-post-content">
                         <h2 class="title text-center">Our Team</h2>
-                    </div>
-                </div>
-                <div class="avatar-post mt-40 mb-80">
-                    <div class="post-avatar-img">
-                        <img src="img/blog/post_avatar_img.png" alt="img">
-                    </div>
-                    <div class="post-avatar-content">
-                        <h5>Andhika Bayu Ramadhany</h5>
-                        <p>Bukan ayang</p>
-                    </div>
-                </div>
-                <div class="avatar-post mt-40 mb-80">
-                    <div class="post-avatar-img">
-                        <img src="img/blog/post_avatar_img.png" alt="img">
-                    </div>
-                    <div class="post-avatar-content">
-                        <h5>Dio Fathir Zinedine Khalaf</h5>
-                        <p>Bukan ayang</p>
-                    </div>
-                </div>
-                <div class="avatar-post mt-40 mb-80">
-                    <div class="post-avatar-img">
-                        <img src="img/blog/post_avatar_img.png" alt="img">
-                    </div>
-                    <div class="post-avatar-content">
-                        <h5>Gloria Loren Lawuredja</h5>
-                        <p>Bukan ayang</p>
-                    </div>
-                </div>
-                <div class="avatar-post mt-40 mb-80">
-                    <div class="post-avatar-img">
-                        <img src="img/blog/post_avatar_img.png" alt="img">
-                    </div>
-                    <div class="post-avatar-content">
-                        <h5>Iqbal Abdurrahman</h5>
-                        <p>Ini adalah saya</p>
-                    </div>
-                </div>
-                <div class="avatar-post mt-40 mb-80">
-                    <div class="post-avatar-img">
-                        <img src="img/blog/post_avatar_img.png" alt="img">
-                    </div>
-                    <div class="post-avatar-content">
-                    <h5>Silmi Julia Rahmi</h5>
-                    <p>Bukan ayang</p>
+                        <div class="avatar-post mt-40 mb-80">
+                            <div class="post-avatar-img">
+                                <img src="img/blog/post_avatar_img.png" alt="img">
+                            </div>
+                            <div class="post-avatar-content">
+                                <h5>Andhika Bayu Ramadhany</h5>
+                                <p>Bukan ayang</p>
+                            </div>
+                        </div>
+                        <div class="avatar-post mt-40 mb-80">
+                            <div class="post-avatar-img">
+                                <img src="img/blog/post_avatar_img.png" alt="img">
+                            </div>
+                            <div class="post-avatar-content">
+                                <h5>Dio Fathir Zinedine Khalaf</h5>
+                                <p>Bukan ayang</p>
+                            </div>
+                        </div>
+                        <div class="avatar-post mt-40 mb-80">
+                            <div class="post-avatar-img">
+                                <img src="img/blog/post_avatar_img.png" alt="img">
+                            </div>
+                            <div class="post-avatar-content">
+                                <h5>Gloria Loren Lawuredja</h5>
+                                <p>Bukan ayang</p>
+                            </div>
+                        </div>
+                        <div class="avatar-post mt-40 mb-80">
+                            <div class="post-avatar-img">
+                                <img src="img/blog/post_avatar_img.png" alt="img">
+                            </div>
+                            <div class="post-avatar-content">
+                                <h5>Iqbal Abdurrahman</h5>
+                                <p>Ini adalah saya</p>
+                            </div>
+                        </div>
+                        <div class="avatar-post mt-40 mb-80">
+                            <div class="post-avatar-img">
+                                <img src="img/blog/post_avatar_img.png" alt="img">
+                            </div>
+                            <div class="post-avatar-content">
+                            <h5>Silmi Julia Rahmi</h5>
+                            <p>Bukan ayang</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
